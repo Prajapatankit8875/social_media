1dea3f4645ccb13d9e7f9e9087e157e97f597c29
prajapatankit8875 <ankitkumawat027@gmail.com>
2025-04-28 15:03:18 +0530

    follow/unfollow

--- userauth/urls.py
+++ userauth/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from userauth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('signup/',views.signup),
-    path('login/',views.login), 
-    path('logout/',views.logout),
+    path('',views.home,name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'), 
+    path('logout/',views.logout,name='logout'),
+    path('upload/',views.upload,name='upload'),
+    path('like-post/<str:id>',views.likes,name='like_post'),
+    path('#<str:id>',views.home_posts),
 ]
--- userauth/views.py
+++ userauth/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
-from . models import Profile
+from . models import Profile, Post, LikePost
 
 # Create your views here.
 def signup(request):
@@ -41,5 +41,49 @@ def logout(request):
     logout(request)
     return redirect('/login')
 
+def upload(request):
+    if request.method=='POST':
+        user = request.user.username
+        image = request.files.get('image-upload')
+        caption = request.POST.get('caption')
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return render('/')
+    
+   
+
 def home(request):
-    return HttpResponse("Hello, world. You're at the userauth home page.")
+    post=Post.objects.all().order_by('-created_at')
+    profile = Profile.objects.all()
+    context = {
+        'post': post,
+        # 'profile': profile
+    } 
+    return render(request, 'main.html',context)
+
+def likes(request, id):
+    if request.method == 'GET':
+        username = request.user.username
+        post = get_object_or_404(Post, id=id)
+
+        like_filter = LikePost.objects.filter(post_id=id, username=username).first()
+        if like_filter is None:
+            new_like = LikePost.objects.create(post_id=id, username=username)
+            post.no_of_likes += 1
+        else:
+            like_filter.delete()
+            post.no_of_likes -= 1 
+    post.save()  
+    return redirect('/#'+id)
+
+def home_posts(request, id):
+    post = Post.objects.get(id=id)
+    profile = Profile.objects.get(user=request.user)
+    context = {
+        'post': post,
+        'profile': profile
+    } 
+    return render(request, 'main.html',context)     
+
